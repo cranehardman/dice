397fb0a6c4c11d5202de23cc653070ca36cc3c7c
Otar Zakalashvili <otar.zakalashvili@gmail.com>
2012-03-29 01:51:57 +0400

    Random Generator Added, TextViews added

--- gen/ge/frender/dice/R.java
+++ gen/ge/frender/dice/R.java
@@ -14,9 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int bAdd=0x7f050001;
-        public static final int bSub=0x7f050002;
-        public static final int tv=0x7f050000;
+        public static final int bRoll=0x7f050002;
+        public static final int tDice1=0x7f050000;
+        public static final int tDice2=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/ge/frender/dice/DiceActivity.java
+++ src/ge/frender/dice/DiceActivity.java
@@ -1,45 +1,43 @@
 package ge.frender.dice;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 public class DiceActivity extends Activity {
 	/** Called when the activity is first created. */
 
-	int counter;
-	Button add, sub;
-	TextView display;
+	Button roll;
+	TextView tdice1;
+	TextView tdice2;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		counter = 0;
-		add = (Button) findViewById(R.id.bAdd);
-		sub = (Button) findViewById(R.id.bSub);
-		display = (TextView) findViewById(R.id.tv);
-		add.setOnClickListener(new View.OnClickListener() {
 
-			@Override
+		roll = (Button) findViewById(R.id.bRoll);
+
+		tdice1 = (TextView) findViewById(R.id.tDice1);
+		tdice2 = (TextView) findViewById(R.id.tDice2);
+		
+		roll.setOnClickListener(new View.OnClickListener() {
+
 			public void onClick(View v) {
-				counter++;
-				display.setText("your total is:" + counter);
-
-			}
-		}); 
-
-		sub.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				counter--;
-				display.setText("your total aaaa is:" + counter);
+				Random r = new Random();
+				int rdice1 = r.nextInt(6)+1;
+				int rdice2 = r.nextInt(6)+1;
+				
+				tdice1.setText("kamateli I: " + rdice1);
+				tdice2.setText("Kamateli II:" + rdice2);
 
 			}
 		});
+		
+
 	}
 }
