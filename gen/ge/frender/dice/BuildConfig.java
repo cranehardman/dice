6d44950b2079883829e0e759f87d3e3845c69cfa
Irakli Soxadze <Linux@linux-471f55.(none)>
2012-03-28 23:08:26 +0400

    econd commit

---
+++ gen/ge/frender/dice/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ge.frender.dice;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ge/frender/dice/DiceActivity.java
@@ -0,0 +1,45 @@
+package ge.frender.dice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class DiceActivity extends Activity {
+	/** Called when the activity is first created. */
+
+	int counter;
+	Button add, sub;
+	TextView display;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		counter = 0;
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display = (TextView) findViewById(R.id.tv);
+		add.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				counter++;
+				display.setText("your total is:" + counter);
+
+			}
+		}); 
+
+		sub.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				counter--;
+				display.setText("your total is:" + counter);
+
+			}
+		});
+	}
+}
