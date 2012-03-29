a55097b1ea120dc32e9737b00df247d0e3bdccb3
Otar Zakalashvili <otar.zakalashvili@gmail.com>
2012-03-29 16:50:26 +0400

    Encapsulated

--- src/ge/frender/dice/DiceActivity.java
+++ src/ge/frender/dice/DiceActivity.java
@@ -3,7 +3,12 @@ package ge.frender.dice;
 import java.util.Random;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,20 +29,23 @@ public class DiceActivity extends Activity {
 
 		tdice1 = (TextView) findViewById(R.id.tDice1);
 		tdice2 = (TextView) findViewById(R.id.tDice2);
-		
 		roll.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				Random r = new Random();
-				int rdice1 = r.nextInt(6)+1;
-				int rdice2 = r.nextInt(6)+1;
-				
-				tdice1.setText("kamateli I: " + rdice1);
-				tdice2.setText("Kamateli II:" + rdice2);
+				roller();
 
 			}
 		});
-		
+
+	}
+
+	private void roller() {
+		Random r = new Random();
+		int rdice1 = r.nextInt(6) + 1;
+		int rdice2 = r.nextInt(6) + 1;
+
+		tdice1.setText("kamateli I: " + rdice1);
+		tdice2.setText("Kamateli II:" + rdice2);
 
 	}
 }
